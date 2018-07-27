7cd29c204668b7e804a0b37e4a30db36e226746d
m081779 <marcoprincipio@hotmail.com>
2018-07-26 22:41:43 -0400

    Second test for click event working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,22 @@
 
 import * as Enzyme from 'enzyme';
-import {shallow} from 'enzyme';
+// import {shallow} from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 import * as React from 'react';
 import App from './App';
+import Button from './components/Button';
 
 
 
 Enzyme.configure({ adapter: new Adapter() });
 describe('<App />', () => {
   it('renders without crashing', () => {
-    shallow(<App />);
+    Enzyme.shallow(<App />);
+  });
+  it('should handle the click event', () => {
+    const mockfunction = jest.fn();
+    const wrapper = Enzyme.shallow(<Button onClick={mockfunction}/>);
+    wrapper.simulate('click');
+    expect(mockfunction).toHaveBeenCalledTimes(1)
   })
 })
